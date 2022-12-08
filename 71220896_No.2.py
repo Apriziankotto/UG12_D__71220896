d8cbb6804edccbc9f9b8aff6dd63a3ae07a12f52
Apriziankotto <117901091+Apriziankotto@users.noreply.github.com>
2022-12-08 15:44:44 +0700

    Add files via upload

---
+++ 71220896_No.2.py
@@ -0,0 +1,17 @@
+print("Pilihan Model Matematika")
+print("1. Perkalian")
+print("2. Pembagian")
+input("Masukkan model matematika yang diinginkan 1/2: ")
+bilangan= int(input("Menampilkan table matematika dari angka:"))
+for a in range (1,11):
+        hasil= bilangan * a
+        print(bilangan, "x",a,"=", hasil)
+
+print("Pilihan Model Matematika")
+print("1. Perkalian")
+print("2. Pembagian")
+input("Masukkan model matematika yang diinginkan 1/2: ")
+bilangan= int(input("Menampilkan table matematika dari angka:"))
+for a in range (50,66):
+        hasil= a / bilangan
+        print(a, ":",bilangan,"=", hasil)
